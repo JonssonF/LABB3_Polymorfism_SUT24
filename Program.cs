659d15df2998fbcf587d0725a7c398a4086af491
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2024-11-03 11:23:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LABB3_Polymorfism_SUT24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
